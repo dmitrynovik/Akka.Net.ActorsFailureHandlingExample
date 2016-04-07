9cb6cc4abe00f58ff5c5aa1a2a7b14cc54650037
dmitrynovik <cryinstone@gmail.com>
2016-04-07 11:54:11 +1000

    Init

---
+++ AkkaTest/Greet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Akka.Actor;
+
+namespace AkkaTest
+{
+    // Create an (immutable) message type that your actor will respond to
+    public class Greet
+    {
+        public Greet(string msg)
+        {
+            Message = msg;
+        }
+
+        public string Message { get; }
+    }
+
+    public class GreetingActorParent : GreetingActor
+    {
+        private readonly IActorRef _child;
+
+        public GreetingActorParent()
+        {
+            _child = Context.ActorOf<GreetingActorChild>("my-only-child");
+        }
+
+        protected override void Handle(Greet greet)
+        {
+            Console.WriteLine("\nParent hello {0}", greet.Message);
+            _child.Forward(greet);
+        }
+    }
+
+    public class GreetingActorChild : GreetingActor
+    {
+        protected override void Handle(Greet greet)
+        {
+            Console.WriteLine("\nChild hello {0}", greet.Message);
+            throw new Exception("The child is dying");
+        }
+    }
+
+    public abstract class GreetingActor : ReceiveActor
+    {
+        protected GreetingActor()
+        {
+            var type = GetType();
+            Console.WriteLine("Creating {0}", type);
+
+            Receive<Greet>(m =>
+            {
+                Handle(m);
+            });
+        }
+
+        protected abstract void Handle(Greet greet);
+    }
+}
---
+++ AkkaTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Akka.Actor;
+
+namespace AkkaTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var system = ActorSystem.Create("my-actors-circus"))
+            {
+                // Create a new actor system (a container for your actors)
+                var greeterParent = system.ActorOf<GreetingActorParent>("greeter");
+
+                // Send a message to the actor
+                greeterParent.Tell(new Greet("world"));
+
+                // This prevents the app from exiting before the async work is done
+                Console.ReadLine();
+            }
+        }
+    }
+}
